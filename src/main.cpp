87fa2917df3d7f99077c08fb7bbf2ce8d4eb87c4
Alexander Sagen <alexander@sagen.me>
2018-02-25 16:45:17 +0100

    Update for latest GMod version

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "GarrysMod/Lua/Interface.h"
-#include "vtable.h"
+#include "vmthook.h"
 #include "util.h"
 #include "windows.h"
 
@@ -9,36 +9,40 @@
 
 typedef unsigned char uchar;
 
-VTable* sharedHooker;
-VTable* clientHooker;
+VMTHook* luaSharedVMTHook;
+VMTHook* luaClientVMTHook;
 
 using namespace GarrysMod;
 
-Lua::ILuaBase *MENU;
-void *clientState;
+Lua::ILuaBase* MENU;
+lua_State* clientState;
+void* rexthisptr;
 
-typedef void *(__thiscall *hRunStringExFn)(void*, char const*, char const*, char const*, bool, bool, bool, bool);
-void * __fastcall hRunStringEx(void *_this, void*, char const* filename, char const* path, char const* torun, bool run, bool showerrors, bool idk, bool idk2)
+typedef void* (__thiscall *RunStringExHookFn)(void* thisptr, const char* fileName, const char* path, const char* stringToRun, bool run, bool showErrors, bool, bool);
+void* __fastcall RunStringExHook(void* thisptr, int edx, const char* fileName, const char* path, const char* stringToRun, bool run, bool showErrors, bool a, bool b)
 {
+	if (rexthisptr == nullptr)
+		rexthisptr = thisptr;
+
 	MENU->PushSpecial(Lua::SPECIAL_GLOB);
 	MENU->GetField(-1, "hook");
 		MENU->GetField(-1, "Call");
 			MENU->PushString("RunOnClient");
 			MENU->PushNil();
-			MENU->PushString(filename);
-			MENU->PushString(torun);
+			MENU->PushString(fileName);
+			MENU->PushString(stringToRun);
 		MENU->Call(4, 1);
 		if (!MENU->IsType(-1, Lua::Type::NIL))
-			torun = MENU->CheckString();
+			stringToRun = MENU->CheckString();
 	MENU->Pop(3);
 
-	return hRunStringExFn(clientHooker->getold(RUNSTRINGEX))(_this, filename, path, torun, run, showerrors, idk, idk2);
+	return luaClientVMTHook->GetOriginalFunction<RunStringExHookFn>(RUNSTRINGEX)(thisptr, fileName, path, stringToRun, run, showErrors, a, b);
 }
 
-typedef void *(__thiscall *hCreateLuaInterfaceFn)(void *, uchar, bool);
-void * __fastcall hCreateLuaInterface(void *_this, void *, uchar stateType, bool renew)
+typedef lua_State* (__thiscall *CreateLuaInterfaceHookFn)(void* thisptr, unsigned char stateType, bool renew);
+void* __fastcall CreateLuaInterfaceHook(void* thisptr, int edx, unsigned char stateType, bool renew)
 {
-	void *state = hCreateLuaInterfaceFn(sharedHooker->getold(CREATELUAINTERFACE))(_this, stateType, renew);
+	lua_State* state = luaSharedVMTHook->GetOriginalFunction<CreateLuaInterfaceHookFn>(CREATELUAINTERFACE)(thisptr, stateType, renew);
 
 	MENU->PushSpecial(Lua::SPECIAL_GLOB);
 	MENU->GetField(-1, "hook");
@@ -53,44 +57,31 @@ void * __fastcall hCreateLuaInterface(void *_this, void *, uchar stateType, bool
 
 	clientState = state;
 
-	clientHooker = new VTable(clientState);
-	clientHooker->hook(RUNSTRINGEX, hRunStringEx);
+	luaClientVMTHook = new VMTHook(state);
+	luaClientVMTHook->HookFunction(&RunStringExHook, RUNSTRINGEX);
 
 	return clientState;
 }
 
-typedef void *(__thiscall *hCloseLuaInterfaceFn)(void*, void*);
-void * __fastcall hCloseLuaInterface(void *_this, void *ukwn, void *luaInterface)
+typedef void* (__thiscall *CloseLuaInterfaceHookFn)(void* thisptr, void* state);
+void* __fastcall CloseLuaInterfaceHook(void* thisptr, int edx, lua_State* state)
 {
-	if (luaInterface == clientState)
+	if (state == clientState) {
 		clientState = NULL;
+		rexthisptr = nullptr;
+	}
 
-	return hCloseLuaInterfaceFn(sharedHooker->getold(CLOSELUAINTERFACE))(_this, luaInterface);
+	return luaSharedVMTHook->GetOriginalFunction<CloseLuaInterfaceHookFn>(CLOSELUAINTERFACE)(thisptr, state);
 }
-
-class CLuaInterface
-{
-private:
-	template<typename t>
-	inline t get(unsigned short which)
-	{
-		return t((*(char ***)(this))[which]);
-	}
-
-public:
-	void RunStringEx(char const* filename, char const* path, char const* torun, bool run = true, bool showerrors = true, bool idk = true, bool idk2 = true)
-	{
-		return get<void(__thiscall *)(void*,char const*, char const*, char const*, bool, bool, bool, bool)>(RUNSTRINGEX)(this, filename, path, torun, run, showerrors, idk, idk2); //free cookies for people that know how to detect stuff
-	}
-
-};
 
 int RunOnClient(lua_State* state)
 {
-	if (!clientState)
+	LUA->CheckType(1, GarrysMod::Lua::Type::STRING);
+
+	if (!clientState || rexthisptr == nullptr)
 		LUA->ThrowError("Not in game");
 
-	reinterpret_cast<CLuaInterface *>(clientState)->RunStringEx(LUA->CheckString(-3), LUA->CheckString(-2), LUA->CheckString());
+	luaClientVMTHook->GetOriginalFunction<RunStringExHookFn>(RUNSTRINGEX)(rexthisptr, "", "", LUA->CheckString(1), true, true, false, false);
 
 	return 0;
 }
@@ -99,16 +90,15 @@ GMOD_MODULE_OPEN()
 {
 	MENU = LUA;
 
-	auto luaShared = util::GetInterfaceSingle<void *>("lua_shared.dll", "LUASHARED003");
+	auto luaShared = util::GetInterfaceSingle<void* >("lua_shared.dll", "LUASHARED003");
 	
 	if (!luaShared)
 		MessageBoxA(NULL, "gay", "gay", NULL);
 
-	sharedHooker = new VTable(luaShared);
+	luaSharedVMTHook = new VMTHook(luaShared);
+	luaSharedVMTHook->HookFunction(&CreateLuaInterfaceHook, CREATELUAINTERFACE);
+	luaSharedVMTHook->HookFunction(&CloseLuaInterfaceHook, CLOSELUAINTERFACE);
 
-	sharedHooker->hook(CREATELUAINTERFACE, hCreateLuaInterface);
-	sharedHooker->hook(CLOSELUAINTERFACE, hCloseLuaInterface);
-	
 	LUA->PushSpecial(Lua::SPECIAL_GLOB);
 	LUA->PushString("RunOnClient");
 	LUA->PushCFunction(RunOnClient);
